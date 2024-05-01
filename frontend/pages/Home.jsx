1167bba96aeeb1c93ae1e3dfc4c422d3fca02ecb
Kate Dinh <79945906+kqteDinh@users.noreply.github.com>
2024-04-30 18:52:06 -0700

    Update Home.jsx
    forgot import

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
+import SendIcon from '@mui/icons-material/Send'
 import Typography from '@mui/material/Typography'
 import { useNavigate } from 'react-router-dom'
 
